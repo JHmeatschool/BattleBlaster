44ed2911d307630b4be2d28193e66da88cb62bef
meatschool <ceojr1234@gmail.com>
2026-02-05 20:52:43 +0900

    feat: restart current level on game over timeout

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -73,6 +73,15 @@ void ABattleBlasterGameMode::ActorDied(AActor* DeadActor)
 	{
 		FString GameOverString = IsVictory ? "Victory!" : "Defeat!";
 
-		UE_LOG(LogTemp, Display, TEXT("Game Over: %s"), *GameOverString);
+		UE_LOG(LogTemp, Display, TEXT("Game over: %s"), *GameOverString);
+
+		FTimerHandle GameOverTimerHandle;
+		GetWorldTimerManager().SetTimer(GameOverTimerHandle, this, &ABattleBlasterGameMode::OnGameOverTimerTimeout, GameOverDelay, false);
 	}
+}
+
+void ABattleBlasterGameMode::OnGameOverTimerTimeout()
+{
+	FString CurrentLevelName = UGameplayStatics::GetCurrentLevelName(GetWorld());
+	UGameplayStatics::OpenLevel(GetWorld(), *CurrentLevelName);
 }
