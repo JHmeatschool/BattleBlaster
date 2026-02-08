e35b927d3b544369b6ff3933590afd2c70da7364
meatschool <ceojr1234@gmail.com>
2026-02-08 21:16:19 +0900

    refactor: remove tick and optimize dependency in HealthComponent

--- Source/BattleBlaster/HealthComponent.h
+++ Source/BattleBlaster/HealthComponent.h
@@ -1,39 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-
-#include "BattleBlasterGameMode.h"
-
 #include "HealthComponent.generated.h"
 
+class ABattleBlasterGameMode;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLEBLASTER_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	UHealthComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-	UPROPERTY(EditAnywhere)
+private:
+	UPROPERTY(EditAnywhere, Category = "Health")
 	float MaxHealth = 100.0f;
 
-	UPROPERTY(VisibleAnywhere)
-	float Health;
-
-	ABattleBlasterGameMode* BBGameMode;
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	float Health = 0.0f;
 
 	UFUNCTION()
 	void OnDamageTaken(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
