2b9bea78d1542a86b419dc3cb4357e13d99132d0
meatschool <ceojr1234@gmail.com>
2026-02-08 21:18:10 +0900

    refactor: remove tick and optimize headers in Projectile class

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -1,57 +1,51 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-
-#include "GameFramework/ProjectileMovementComponent.h"
-
-#include "NiagaraComponent.h"
-#include "NiagaraFunctionLibrary.h"
-
 #include "Projectile.generated.h"
+
+class UProjectileMovementComponent;
+class UStaticMeshComponent;
+class UNiagaraComponent;
+class UNiagaraSystem;
+class USoundBase;
+class UCameraShakeBase;
 
 UCLASS()
 class BATTLEBLASTER_API AProjectile : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AProjectile();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(VisibleAnywhere)
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* ProjectileMesh;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovementComp;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
 	UNiagaraComponent* TrailParticles;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	UNiagaraSystem* HitParticles;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	USoundBase* LaunchSound;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	USoundBase* HitSound;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	TSubclassOf<UCameraShakeBase> HitCameraShakeClass;
 
-	UPROPERTY(EditAnywhere)
-	float Damage = 25.0f;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	float Damage = 50.0f;
 
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
